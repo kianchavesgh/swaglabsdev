9a9d5bd90bacd18d5ceae9f2c7184b83181dd432
Kian Chaves <kianchaves@live.com>
2025-08-07 19:25:28 -0300

    artifact

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -8,7 +8,7 @@ describe('Testes de Login - Swag Labs', () => {
   });
 
   it('TC-LOGIN-001 - Autenticação com usuário válido', () => {
-    cy.get('[data-test="username"]').type('standard_user'); // insere username
+    cy.get('[data-test="username"]').type('standard_user'); // insere username ** é esperadoa
     cy.get('[data-test="password"]').type('secret_sauce');   // insere senha
     cy.get('#login-button').click();            // clica no botão de login
     // cy.url().should('include', '/inventory.html'); // verifica redirecionamento maneira ideal
